38b00a933c7dcfff1b220678f184d44e284668df
승호 <tmdgh717@naver.com>
2025-03-21 13:26:53 +0900

    파일 입출력과 성능 최적화2 - 버퍼 활용

---
+++ src/io/buffered/ReadFileV2.java
@@ -0,0 +1,29 @@
+package io.buffered;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static io.buffered.BufferedConst.BUFFER_SIZE;
+import static io.buffered.BufferedConst.FILE_NAME;
+import static io.buffered.BufferedConst.FILE_SIZE;
+
+public class ReadFileV2 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream(FILE_NAME);
+        long startTime = System.currentTimeMillis();
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int fileSize = 0;
+        int size;
+        while ((size = fis.read(buffer)) != -1) {
+            fileSize += size;
+        }
+        fis.close();
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("File create: " + FILE_NAME);
+        System.out.println("File size: " + FILE_SIZE / 1024 / 1024 + "MB");
+        System.out.println("Time taken: " + (endTime - startTime) + "ms"); // 6ms
+    }
+}
