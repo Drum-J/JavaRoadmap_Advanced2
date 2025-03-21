d143af0b093a7cea9064883cba0bb774e519972f
승호 <tmdgh717@naver.com>
2025-03-21 13:51:42 +0900

    파일 입출력과 성능 최적화4 - Buffered 스트림 읽기

---
+++ src/io/buffered/ReadFileV3.java
@@ -0,0 +1,31 @@
+package io.buffered;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import static io.buffered.BufferedConst.BUFFER_SIZE;
+import static io.buffered.BufferedConst.FILE_NAME;
+import static io.buffered.BufferedConst.FILE_SIZE;
+
+public class ReadFileV3 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream(FILE_NAME);
+        BufferedInputStream bis = new BufferedInputStream(fis, BUFFER_SIZE);
+
+        long startTime = System.currentTimeMillis();
+
+        int fileSize = 0;
+        int size;
+        while ((size = bis.read()) != -1) { // bis에서 읽고
+            fileSize++;
+        }
+        bis.close(); // bis에서 닫아야한다.
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("File create: " + FILE_NAME);
+        System.out.println("File size: " + FILE_SIZE / 1024 / 1024 + "MB");
+        System.out.println("Time taken: " + (endTime - startTime) + "ms"); // 145ms
+    }
+}
