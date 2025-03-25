6123708b6b9b38eeedbce3ee16557215c11fdefe
승호 <102205699+Drum-J@users.noreply.github.com>
2025-03-25 11:46:27 +0900

    04.I/O 활용 완료 (#4)
    * 회원 관리 예제1 - 메모리
    
    * 회원 관리 예제2 - 파일에 보관
    
    * 회원 관리 예제3 - DataStream
    
    * 회원 관리 예제4 - ObjectStream

---
+++ src/io/member/MemberConsoleMain.java
@@ -0,0 +1,67 @@
+package io.member;
+
+import io.member.impl.DataMemberRepository;
+import io.member.impl.FileMemberRepository;
+import io.member.impl.MemoryMemberRepository;
+import io.member.impl.ObjectMemberRepository;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class MemberConsoleMain {
+
+    //private static final MemberRepository repository = new MemoryMemberRepository();
+    //private static final MemberRepository repository = new FileMemberRepository();
+    //private static final MemberRepository repository = new DataMemberRepository();
+    private static final MemberRepository repository = new ObjectMemberRepository();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.println("1. 회원 등록 | 2. 회원 목록 조회 | 3. 종료");
+            System.out.print("선택: ");
+
+            int choice = Integer.parseInt(br.readLine());
+
+            switch (choice) {
+                case 1 :
+                    registerMember(br);
+                    break;
+                case 2 :
+                    displayMembers();
+                    break;
+                case 3:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                default :
+                    System.out.println("잘못된 선택 입니다. 다시 선택하세요.");
+            }
+        }
+    }
+
+    private static void registerMember(BufferedReader br) throws IOException {
+        System.out.print("ID 입력: ");
+        String id = br.readLine();
+
+        System.out.print("Name 입력: ");
+        String name = br.readLine();
+
+        System.out.print("Age 입력: ");
+        int age = Integer.parseInt(br.readLine());
+
+        Member newMember = new Member(id, name, age);
+        repository.add(newMember);
+        System.out.println("회원이 성공적으로 등록 되었습니다: " + newMember);
+    }
+
+    private static void displayMembers() {
+        List<Member> members = repository.findAll();
+        System.out.println("회원 목록 : ");
+        for (Member member : members) {
+            System.out.printf("[ID: %s, Name: %s, Age: %d]\n", member.getId(), member.getName(), member.getAge());
+        }
+    }
+}
