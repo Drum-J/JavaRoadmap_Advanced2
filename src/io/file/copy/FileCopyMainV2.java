3b22d28582edbf41d5069c074e59bd69f112c486
승호 <102205699+Drum-J@users.noreply.github.com>
2025-03-26 10:06:41 +0900

    05.File, Files 완료 (#5)
    * File
    
    * Files
    
    * 경로 표시
    
    * Files로 문자 파일 읽기
    
    * 파일 복사 최적화

---
+++ src/io/file/copy/FileCopyMainV2.java
@@ -0,0 +1,21 @@
+package io.file.copy;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileCopyMainV2 {
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+        FileInputStream fis = new FileInputStream("temp/copy.dat");
+        FileOutputStream fos = new FileOutputStream("temp/copy_new.dat");
+
+        fis.transferTo(fos); //인풋스트림(fis)을 읽어서 아웃풋스트림(fos)으로 전송
+
+        fis.close();
+        fos.close();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time taken: " + (endTime - startTime) + "ms"); // 143ms
+    }
+}
